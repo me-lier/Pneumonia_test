1f39cf55dba0351d388eb824d70ac2acc604667d
Srinivas Deekonda <126309422+me-lier@users.noreply.github.com>
2025-02-11 18:00:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from PIL import Image, ImageOps
 import numpy as np
 
 # Load the model
-model = load_model("keras_Model.h5", compile=False)
+model = load_model("keras_model.h5", compile=False)
 
 # Load the labels
 class_names = open("labels.txt", "r").readlines()
